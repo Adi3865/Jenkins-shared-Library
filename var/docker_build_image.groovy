df396434d556e4d17db6d08ab1839efd7a44ed8f
Adi3865 <106516113+Adi3865@users.noreply.github.com>
2026-09-25 00:06:10 +0530

    Add ProjectName parameter to docker build function

--- var/docker_build_image.groovy
+++ var/docker_build_image.groovy
@@ -1,3 +1,3 @@
-def  call(String ImageTage, String DockerHubUser) {
+def  call(String ImageTage, String DockerHubUser, String ProjectName) {
 sh " docker build -t ${DockerHubUser}/${ProjectName}:${ImageTage} ."
 }
